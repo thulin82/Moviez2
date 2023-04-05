576a02c0b27604003032c4e3d41022f6b44712be
Markus Thulin <macky_b@hotmail.com>
2023-04-05 21:47:25 +0200

    Started to add components

---
+++ src/Movies/Movies.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Movies.css";
+
+class Movies extends React.Component {
+    render() {
+        return <section className="movies">Movies</section>;
+    }
+}
+
+export default Movies;
