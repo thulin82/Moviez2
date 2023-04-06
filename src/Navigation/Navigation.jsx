e81b919277bc1908b0ad9afe6a2dead00f7279a9
Markus Thulin <macky_b@hotmail.com>
2023-04-06 16:12:31 +0200

    Moved change of genre

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,13 +1,18 @@
+import React, { useState } from "react";
 import "./Navigation.css";
 import Selection from "./Selection";
 
 const Navigation = () => {
+    const [genre, setGenre] = useState("comedy");
+
+    const onGenreChange = (event) => {
+        setGenre(event.target.value);
+    };
+
     return (
-        <>
-            <section className="navigation">
-                <Selection />
-            </section>
-        </>
+        <section className="navigation">
+            <Selection genre={genre} onGenreChange={onGenreChange} />
+        </section>
     );
 };
 
--- src/Navigation/Selection.jsx
+++ src/Navigation/Selection.jsx
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
 import "./Selection.css";
 
-const Selection = () => {
-    const [genre, setGenre] = useState("comedy");
-
-    const onGenreChange = (event) => {
-        setGenre(event.target.value);
-    };
-
-    return (
-        <div className="selection">
-            <label>Genre</label>
-            <select value={genre} onChange={onGenreChange}>
-                <option value="comedy">Comedy</option>
-                <option value="action">Action</option>
-                <option value="drama">Drama</option>
-            </select>
-        </div>
-    );
-};
+const Selection = ({ genre, onGenreChange }) => (
+    <div className="selection">
+        <label>Genre</label>
+        <select value={genre} onChange={onGenreChange}>
+            <option value="comedy">Comedy</option>
+            <option value="action">Action</option>
+            <option value="drama">Drama</option>
+        </select>
+    </div>
+);
 
 export default Selection;
