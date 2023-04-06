02a4e96d826997475ffab0f56df6130cd47ffab5
Markus Thulin <macky_b@hotmail.com>
2023-04-06 08:30:08 +0200

    Added MovieListItem and initial API call in Movies

---
+++ src/Movies/MovieListitem.jsx
@@ -0,0 +1,5 @@
+import "./MovieListItem.css";
+
+const MovieListItem = ({ title }) => <li className="movie-item">{title}</li>;
+
+export default MovieListItem;
--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import "./Movies.css";
+import MovieListItem from "./MovieListitem";
+
+const movies = ["The Godfather", "The Shawshank Redemption", "The Dark Knight"];
 
 const Movies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${
+            import.meta.env.VITE_TMDB_API_KEY
+        }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => setMovies(data.results))
+            .catch((error) => console.log(error));
+
+        console.log("Before or after data?");
+    }, []);
+
     return (
-        <>
-            <section className="movies">Movies</section>
-        </>
+        <section>
+            <ul className="movies">
+                {movies.map((movie, index) => (
+                    <MovieListItem key={index.toString()} title={movie.title} />
+                ))}
+            </ul>
+        </section>
     );
 };
 
